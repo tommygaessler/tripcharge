cfd0bbac70f44d6c170b720c64321beb762b9dc4
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-11-17 18:49:47 -0700

    added validation to user inputs

--- ChargingStations/ViewController.swift
+++ ChargingStations/ViewController.swift
@@ -6,14 +6,10 @@
 //  Copyright © 2016 Tommy Gaessler. All rights reserved.
 //
 // Goals:
-// 2. Use current location as option for finding closest charger
 // 3. Use current location as option for starting point
 // 4. When you click on a pin there will be a link to route to it and open google maps
 //
 // Stretch:
-// 1. Change inputs to accept Addresses instead of cords (mostly server side work)
-// 2. Add route line from starting point to destination that is fatest and hits most chargers
-// 3. Add route line to closest charger when finding nearest charger
 // 4. Add ability to send put post and delete requests to open charge api if charging info needs to be updated
 // 5. See how many charging spots are currently availble when you click on pin
 // 6. See how many total charging spots there are
@@ -27,15 +23,15 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate, UITextFieldDelegate, GMSMapViewDelegate {
     
-    // MARK: Find Closest Stations Inputs
-    
-    @IBOutlet weak var CurrentLat: UITextField!
-    @IBOutlet weak var CurrentLong: UITextField!
-    
     // MARK: Find Stations Along Route Inputs
     
     @IBOutlet weak var StartAddress: UITextField!
     @IBOutlet weak var EndAddress: UITextField!
+    
+    let locationManager = CLLocationManager()
+    
+    var latitude = 0.00
+    var Longitude = 0.00
     
     // MARK: Properties
     
@@ -44,6 +40,27 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITextFieldDe
         // Do any additional setup after loading the view, typically from a nib.
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
+        
+        locationManager.delegate = self
+        locationManager.requestLocation()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location11 = locations.first {
+            latitude = location11.coordinate.latitude
+            Longitude = location11.coordinate.longitude
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        let alertController = UIAlertController(title: "ChargingStations", message:
+            "In order to find closest stations, go to settings and enable location services :)", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+        
+        self.present(alertController, animated: true, completion: nil)
     }
     
     func dismissKeyboard() {
@@ -60,67 +77,49 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITextFieldDe
     
     @IBAction func FindClosestStations(_ sender: UIButton) {
         
-        // MARK: Hides keyboard on submit
-        self.view.endEditing(true)
-        
-        // MARK: Check if input is empty or invalid
-        
-        if CurrentLat.text!.isEmpty || CurrentLong.text!.isEmpty || NumberFormatter().number(from: CurrentLat.text!) == nil || NumberFormatter().number(from: CurrentLong.text!) == nil {
+        // MARK: Get Screen Size
+        let screenSize: CGRect = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
             
-            let alertController = UIAlertController(title: "ChargingStations", message:
-                "Please Enter Valid Coordinates", preferredStyle: UIAlertControllerStyle.alert)
-            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+        // MARK: Google Maps
+        let camera = GMSCameraPosition.camera(withLatitude: latitude, longitude: Longitude, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect(x: 0, y: 60, width: screenWidth, height: screenHeight - 60), camera: camera)
+        self.view.addSubview(mapView)
             
-            self.present(alertController, animated: true, completion: nil)
-        } else {
+        //MARK: Set current location pin
+        let position3 = CLLocationCoordinate2DMake(latitude, Longitude)
+        let marker3 = GMSMarker(position: position3)
+        marker3.title = "Search Location"
+        marker3.icon = GMSMarker.markerImage(with: UIColor.green)
+        marker3.map = mapView
             
-            let currentLatInput = Double(CurrentLat.text!)
-            let currentLongInput = Double(CurrentLong.text!)
-            
-            // MARK: Get Screen Size
-            let screenSize: CGRect = UIScreen.main.bounds
-            let screenWidth = screenSize.width
-            let screenHeight = screenSize.height
-            
-            // MARK: Google Maps
-            let camera = GMSCameraPosition.camera(withLatitude: currentLatInput!, longitude: currentLongInput!, zoom: 6.0)
-            let mapView = GMSMapView.map(withFrame: CGRect(x: 0, y: 100, width: screenWidth, height: screenHeight - 100), camera: camera)
-            self.view.addSubview(mapView)
-            
-            //MARK: Set current location pin
-            let position3 = CLLocationCoordinate2DMake(currentLatInput!, currentLongInput!)
-            let marker3 = GMSMarker(position: position3)
-            marker3.title = "Search Location"
-            marker3.icon = GMSMarker.markerImage(with: UIColor.green)
-            marker3.map = mapView
-            
-            //MARK: Send Location to Austins API
-            Alamofire.request("https://guarded-garden-39811.herokuapp.com/lat/\(currentLatInput! as Double)/long/\(currentLongInput! as Double)").responseJSON { response in
+        //MARK: Send Location to Austins API
+        Alamofire.request("https://guarded-garden-39811.herokuapp.com/lat/\(latitude)/long/\(Longitude)").responseJSON { response in
                 
-                if (response.result.value as! Array<Any>).isEmpty {
-                    let alertController = UIAlertController(title: "ChargingStations", message:
+            if (response.result.value as! Array<Any>).isEmpty {
+                let alertController = UIAlertController(title: "ChargingStations", message:
                         "No Stations Found", preferredStyle: UIAlertControllerStyle.alert)
-                    alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
                     
-                    self.present(alertController, animated: true, completion: nil)
-                } else {
+                self.present(alertController, animated: true, completion: nil)
+            } else {
                     
-                    //MARK: Add pins for each charging station location
-                    if let addresses = response.result.value {
-                        for address in addresses as! [AnyObject] {
+                //MARK: Add pins for each charging station location
+                if let addresses = response.result.value {
+                    for address in addresses as! [AnyObject] {
                             
-                            let info = address["AddressInfo"] as! [String : AnyObject]
+                        let info = address["AddressInfo"] as! [String : AnyObject]
                             
-                            let addressLine1 = info["AddressLine1"] as! String
-                            let lat = info["Latitude"] as! Double
-                            let long = info["Longitude"] as! Double
+                        let addressLine1 = info["AddressLine1"] as! String
+                        let lat = info["Latitude"] as! Double
+                        let long = info["Longitude"] as! Double
                             
-                            let position = CLLocationCoordinate2DMake(lat, long)
-                            let marker = GMSMarker(position: position)
-                            marker.title = addressLine1
-                            marker.snippet = "Take Me Here"
-                            marker.map = mapView
-                        }
+                        let position = CLLocationCoordinate2DMake(lat, long)
+                        let marker = GMSMarker(position: position)
+                        marker.title = addressLine1
+                        marker.snippet = "Take Me Here"
+                        marker.map = mapView
                     }
                 }
             }
@@ -158,8 +157,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITextFieldDe
                 
                 guard response.result.error == nil else {
                     // got an error in getting the data, need to handle it
-                    print("error")
-                    print(response.result.error!)
+                    let alertController = UIAlertController(title: "ChargingStations", message:
+                        "Invalid Locations", preferredStyle: UIAlertControllerStyle.alert)
+                    alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+                    
+                    self.present(alertController, animated: true, completion: nil)
                     return
                 }
                 
@@ -167,41 +169,49 @@ class ViewController: UIViewController, CLLocationManagerDelegate, UITextFieldDe
                     
                     let addresses = JSON(data)
                     
-                    let startLat = addresses[addresses.count-1]["startingEndCords"]["startLat"].double
-                    let startLng = addresses[addresses.count-1]["startingEndCords"]["startLng"].double
-                    let endLat = addresses[addresses.count-1]["startingEndCords"]["endLat"].double
-                    let endLng = addresses[addresses.count-1]["startingEndCords"]["endLng"].double
-                    
-                    // MARK: Google Maps
-                    let camera = GMSCameraPosition.camera(withLatitude: startLat!, longitude: startLng!, zoom: 6.0)
-                    let mapView = GMSMapView.map(withFrame: CGRect(x: 0, y: 100, width: screenWidth, height: screenHeight-100), camera: camera)
-                    self.view.addSubview(mapView)
-                    
-                    //MARK: Set starting pin
-                    let position1 = CLLocationCoordinate2DMake(startLat!, startLng!)
-                    let marker1 = GMSMarker(position: position1)
-                    marker1.title = "Starting Location"
-                    marker1.icon = GMSMarker.markerImage(with: UIColor.green)
-                    marker1.map = mapView
-                    
-                    //MARK: Set destination pin
-                    let position2 = CLLocationCoordinate2DMake(endLat!, endLng!)
-                    let marker2 = GMSMarker(position: position2)
-                    marker2.title = "Destination"
-                    marker2.icon = GMSMarker.markerImage(with: UIColor.blue)
-                    marker2.map = mapView
-                    
-                    for index in 0...addresses.count-2 {
+                    if addresses.count == 0 {
+                        let alertController = UIAlertController(title: "ChargingStations", message:
+                            "No Stations Found", preferredStyle: UIAlertControllerStyle.alert)
+                        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
                         
-                        let lat = addresses[index]["AddressInfo"]["Latitude"].double
-                        let lng = addresses[index]["AddressInfo"]["Longitude"].double
-                        let address = addresses[index]["AddressInfo"]["AddressLine1"].string
+                        self.present(alertController, animated: true, completion: nil)
+                    } else {
+                        let startLat = addresses[addresses.count-1]["startingEndCords"]["startLat"].double
+                        let startLng = addresses[addresses.count-1]["startingEndCords"]["startLng"].double
+                        let endLat = addresses[addresses.count-1]["startingEndCords"]["endLat"].double
+                        let endLng = addresses[addresses.count-1]["startingEndCords"]["endLng"].double
                         
-                        let position = CLLocationCoordinate2DMake(lat!, lng!)
-                        let marker = GMSMarker(position: position)
-                        marker.title = address
-                        marker.snippet = "Take Me Here"
-                        marker.map = mapView
+                        // MARK: Google Maps
+                        let camera = GMSCameraPosition.camera(withLatitude: startLat!, longitude: startLng!, zoom: 6.0)
+                        let mapView = GMSMapView.map(withFrame: CGRect(x: 0, y: 100, width: screenWidth, height: screenHeight-100), camera: camera)
+                        self.view.addSubview(mapView)
+                        
+                        //MARK: Set starting pin
+                        let position1 = CLLocationCoordinate2DMake(startLat!, startLng!)
+                        let marker1 = GMSMarker(position: position1)
+                        marker1.title = "Starting Location"
+                        marker1.icon = GMSMarker.markerImage(with: UIColor.green)
+                        marker1.map = mapView
+                        
+                        //MARK: Set destination pin
+                        let position2 = CLLocationCoordinate2DMake(endLat!, endLng!)
+                        let marker2 = GMSMarker(position: position2)
+                        marker2.title = "Destination"
+                        marker2.icon = GMSMarker.markerImage(with: UIColor.blue)
+                        marker2.map = mapView
+                        
+                        for index in 0...addresses.count-2 {
+                            
+                            let lat = addresses[index]["AddressInfo"]["Latitude"].double
+                            let lng = addresses[index]["AddressInfo"]["Longitude"].double
+                            let address = addresses[index]["AddressInfo"]["AddressLine1"].string
+                            
+                            let position = CLLocationCoordinate2DMake(lat!, lng!)
+                            let marker = GMSMarker(position: position)
+                            marker.title = address
+                            marker.snippet = "Take Me Here"
+                            marker.map = mapView
+                        }
                     }
                 }
             }
